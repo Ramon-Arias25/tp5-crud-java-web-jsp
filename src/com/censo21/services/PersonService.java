f727a14aa8ea29a445aadb6965ba69fc65d70e8d
Ramon Arias <reab987@gmail.com>
2021-05-26 13:39:13 -0300

    add comments to almost PersonService classes

--- src/com/censo21/services/PersonService.java
+++ src/com/censo21/services/PersonService.java
@@ -18,16 +18,28 @@ import com.censo21.repository.PersonRepository;
  * current version: 1
  */
 public class PersonService {
-
+	/**
+	 * Declara un objeto de tipo PersonRepository
+	 */
 	PersonRepository personRepository;
-
+	/**
+	 * Se inicializa el objeto PersonRepository
+	 */
 	public PersonService() {
 		personRepository = new PersonRepository();
 	}
-
+	/**
+	 * Llama al metodo create de PeronRepository para insertar en la base de datos
+	 * @param un objeto de tipo Person
+	 * @return true o false segun retorne el metodo PeronRepository.create
+	 */
 	public boolean create(Person person) {
 		return personRepository.create(person);
 	}
+	/**
+	 * Llama al metodo getAll de PeronRepository para solicitar todas las personas en la base de datos
+	 * @return una lista de tipo Person
+	 */
 
 	public List<Person> getAll() {
 		try {
@@ -37,7 +49,11 @@ public class PersonService {
 			return null;
 		}
 	}
-
+	/**
+	 * Llama al metodo update de PeronRepository para actualizar los datos de una persona en la base de datos
+	 * @param un objeto de tipo Person
+	 * @return true o false segun retorne el metodo PeronRepository.update
+	 */
 	public boolean update(Person person) {
 		boolean updated = false;
 		try {
@@ -48,54 +64,85 @@ public class PersonService {
 		}
 		return updated;
 	}
-
-	public boolean delete(String DocumentType, String DocumentNumber) {
+	/**
+	 * Llama al metodo delete de PeronRepository para eliminar los datos de una persona en la base de datos
+	 * @param documentType de tipo String, documentNumber de tipo String
+	 * @return true o false segun retorne el metodo PeronRepository.delete
+	 */
+	public boolean delete(String documentType, String documentNumber) {
 		boolean isDeleted = false;
 		try {
-			isDeleted = personRepository.delete(DocumentType, DocumentNumber);
+			isDeleted = personRepository.delete(documentType, documentNumber);
 		} catch (Censo21Exceptions e) {
 			System.out.println("ERROR: " + e.getMessage());
 		}
 		return isDeleted;
 	}
-
-	public Person getOne(String DocumentType, String DocumentNumber) {
+	/**
+	 * Llama al metodo get de PeronRepository para soliciar una persona de la base de datos
+	 * @param documentType de tipo String, documentNumber de tipo String
+	 * @return un objeto de tipo Person
+	 */
+	public Person getOne(String documentType, String documentNumber) {
 		Person person = null;
 		try {
-			person = personRepository.get(DocumentType, DocumentNumber);
+			person = personRepository.get(documentType, documentNumber);
 		} catch (Exception e) {
 			System.out.println("ERROR: " + e.getMessage());
 		}
 		return person;
 	}
-
-	public boolean ReportFile(String file) {
+	/**
+	 * Llama al metodo getAllbyLastName de PeronRepository 
+	 * y al metodo generateReportFile para generar el archivo personas_por_orden_de_apellido.txt
+	 * @param reportType de tipo String
+	 * @return true o false segun retorne el metodo getAllByLastName
+	 */
+	public boolean ReportFile(String reportType) {
 		boolean generated = false;
 		List<Person> persons = personRepository.getAllByLastName();
-		generated = generateReportFile(persons, file);
+		generated = generateReportFile(persons, reportType);
 		return generated;
 	}
-
-	public boolean reportAdultList(String file) {
+	/**
+	 * Llama al metodo getAllAdultList de PeronRepository 
+	 * y al metodo generateReportFile para generar el archivo personas_mayores_de_18.txt
+	 * @param reportType de tipo String
+	 * @return true o false segun retorne el metodo getAllAdultList
+	 */
+	public boolean reportAdultList(String reportType) {
 		boolean generated = false;
 		List<Person> persons = personRepository.getAllAdultList();
-		generated = generateReportFile(persons, file);
+		generated = generateReportFile(persons, reportType);
 		return generated;
 	}
-
-	public boolean reportPovertyLine(String file) {
+	/**
+	 * Llama al metodo getAllPovertyLine de PeronRepository 
+	 * y al metodo generateReportFile para generar el archivo personas_con_ingresos_por_debajo_del_5000.txt
+	 * @param reportType de tipo String
+	 * @return true o false segun retorne el metodo getAllPovertyLine
+	 */
+	public boolean reportPovertyLine(String reportType) {
 		boolean generated = false;
 		List<Person> persons = personRepository.getAllPovertyLine();
-		generated = generateReportFile(persons, file);
+		generated = generateReportFile(persons, reportType);
 		return generated;
 	}
-
+	/**
+	 * Llama al metodo getCountByGener de PeronRepository 
+	 * para generar el archivo personas_con_ingresos_por_debajo_del_5000.txt
+	 * @return true o false segun retorne el metodo getCountByGener
+	 */
 	public boolean reportCountByGender() {
 		boolean generated = false;
 		generated = generateCountByGender();
 		return generated;
 	}
-
+	/**
+	 * Crea un nuevo archivo
+	 * @param nombre del archivo de tipo String
+	 * @return referencia del archivo a crear de tipo File
+	 */
 	public File fileExists(String fileName) {
 
 		File file = new File("C:/reportes_censo/", fileName);
@@ -106,7 +153,10 @@ public class PersonService {
 		}
 		return file;
 	}
-
+	/**
+	 * Crea un nuevo archivo conteo_de_personas_por_sexo.txt
+	 * @return true o flase si es creado
+	 */
 	public boolean generateCountByGender() {
 		boolean generated = false;
 		try {
@@ -126,7 +176,14 @@ public class PersonService {
 		}
 		return generated;
 	}
-
+	/**
+	 * Crea un nuevo archivo de tres opciones segun parametro
+	 * "reportForLastName" --> "personas_por_orden_de_apellido.txt"
+	 * "reportAdultList" --> "personas_mayores_de_18.txt"
+	 * "reportPovertyLine" --> "personas_con_ingresos_por_debajo_del_5000.txt"
+	 * @param  "reportForLastName", "reportAdultList" ó  "reportPovertyLine" tipo String
+	 * @return true o flase si es creado
+	 */
 	public boolean generateReportFile(List<Person> persons, String reportName) {
 		Map<String, String> fileName = new HashMap<>();
 		fileName.put("reportForLastName", "personas_por_orden_de_apellido.txt");
